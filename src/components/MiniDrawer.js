e38e3bdbd9bea897fcfae955de20b42cfe9d8cff
Raymond <wrg7yu@gmail.com>
2020-10-18 02:55:31 -0400

    Add login skeleton, table skeleton, and stylize app bar

--- src/components/MiniDrawer.js
+++ src/components/MiniDrawer.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -8,10 +7,6 @@ import List from '@material-ui/core/List';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -21,100 +16,91 @@ import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import AssignmentIcon from '@material-ui/icons/Assignment';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
-const drawerWidth = 300;
+import Dashboard from './Dashboard'
+import DataTable from './DataTable'
 
+const drawerWidth = 225;
+const barHeight = 75;
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     backgroundColor: "F9F9F9",
   },
   appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
+    minHeight: barHeight,
     width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginRight: 36,
-  },
-  hide: {
-    display: 'none',
+    marginLeft: drawerWidth,
+    backgroundColor: '#01C9E1'
   },
   drawer: {
     width: drawerWidth,
-    height: 100,
     flexShrink: 0,
-    whiteSpace: 'nowrap',
   },
-  drawerOpen: {
+  drawerPaper: {
+    borderStyle: 'none',
     width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerClose: {
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: theme.spacing(7) + 1,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing(9) + 1,
-    },
   },
   toolbar: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
+    minHeight: barHeight,
+  },
+  listHead: {
+    padding: '5px 15px 15px 15px',
+    borderStyle: 'none',
+    backgroundColor: '#01C9E1',
+    height: barHeight,
   },
   content: {
     flexGrow: 1,
-    padding: theme.spacing(3),
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(2),
   },
   listItem: {
-    borderBottom:  '1px solid lightgrey',
+    backgroundColor: theme.palette.background.default,
   },
   selected: {
     backgroundColor: 'lightgrey',
   },
+  logo: {
+    width: '100%',
+  },
   list: {
     flexGrow: 1,
     padding: 0,
+    backgroundColor: theme.palette.background.default,
+    borderRight: '1px solid rgba(0, 0, 0, 0.12)',
   },
   logout: {
     position: 'absolute',
     bottom: 0,
+    backgroundColor: theme.palette.background.default,
   }
 }));
 
 export default function MiniDrawer() {
   const classes = useStyles();
-  const theme = useTheme();
-  const [open, setOpen] = React.useState(false);
+  // const theme = newTheme;
   const [item, setItem] = React.useState(0);
   const handleSelectItem = (index) => {
     setItem(index);
-  }
-  const handleDrawerOpen = () => {
-    setOpen(true);
   };
-
-  const handleDrawerClose = () => {
-    setOpen(false);
+  const renderSwitch = (option) => {
+    switch(option) {
+      case 0:
+        return <Dashboard />;
+      case 1:
+        return <DataTable option={option}/>;
+      case 2:
+        return <DataTable option={option}/>;
+      case 3:
+        return <DataTable option={option}/>;
+      default:
+        return <Typography paragraph>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+          gravida rutrum quisque non tellus. 
+        </Typography>
+    }
   };
   const chooseIcon = (index) => {
     switch(index) {
@@ -129,50 +115,27 @@ export default function MiniDrawer() {
       default:
         return null;
     }
-  }
+  };
   return (
     <div className={classes.root}>
       <CssBaseline />
-      <AppBar
-        position="fixed"
-        className={clsx(classes.appBar, {
-          [classes.appBarShift]: open,
-        })}
-      >
-        <Toolbar>
-          <IconButton
-            color="inherit"
-            aria-label="open drawer"
-            onClick={handleDrawerOpen}
-            edge="start"
-            className={clsx(classes.menuButton, {
-              [classes.hide]: open,
-            })}
-          >
-            <MenuIcon />
-          </IconButton>
+      <AppBar position="fixed" className={classes.appBar}>
+        <Toolbar disableGutters={true}>
           <Typography variant="h6" noWrap>
-            Laundr Portal
+            Portal
           </Typography>
         </Toolbar>
       </AppBar>
       <Drawer
+        className={classes.drawer}
         variant="permanent"
-        className={clsx(classes.drawer, {
-          [classes.drawerOpen]: open,
-          [classes.drawerClose]: !open,
-        })}
         classes={{
-          paper: clsx({
-            [classes.drawerOpen]: open,
-            [classes.drawerClose]: !open,
-          }),
+          paper: classes.drawerPaper,
         }}
+        anchor="left"
       >
-        <div className={classes.toolbar}>
-          <IconButton onClick={handleDrawerClose}>
-            {theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
-          </IconButton>
+        <div className={`${classes.toolbar} ${classes.listHead}`}>
+          <img src="/logo.png" alt="logo" className={classes.logo}/>
         </div>
         <Divider />
         <List className={classes.list}>
@@ -196,29 +159,7 @@ export default function MiniDrawer() {
       </Drawer>
       <main className={classes.content}>
         <div className={classes.toolbar} />
-        <Typography paragraph>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-          facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-          gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-          donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-          adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-          Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-          imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-          arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-          donec massa sapien faucibus et molestie ac.
-        </Typography>
-        <Typography paragraph>
-          Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-          facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-          tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-          consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-          vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-          hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-          tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-          nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-          accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+        {renderSwitch(item)}
       </main>
     </div>
   );
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -7,7 +7,6 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
-import { CenterFocusStrong } from '@material-ui/icons';
 
 
 const useStyles = makeStyles((theme) => ({
