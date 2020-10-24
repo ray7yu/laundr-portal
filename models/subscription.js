ce0fca038d613ed1b107b5e5897e7a5b37dad978
Raymond <wrg7yu@gmail.com>
2020-10-23 23:24:48 -0400

    Change model schemas, add proper date and time sorting, stylize status cells

--- models/subscription.js
+++ models/subscription.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 const SubscriptionSchema = new mongoose.Schema({
     customerName: {type: String, required: true, unique: true},
     subscriptionType: {type: String, required: true},
-    startDate: { type: Date, required: true},
-    renewalDate: {type: Date, required: true},
+    startDate: { type: String, required: true},
+    renewalDate: {type: String, required: true},
     maxLbs: {type: Number, required: true},
     currentLbs: { type: Number, required: true},
     isActive: {type: String, required: true}
